b5350926080c3eee434702b9148a44042caa8bfb
Matthias Diester <mddiester@gmail.com>
2019-02-01 13:28:12 +0100

    Update compare.go

--- internal/cmd/compare.go
+++ internal/cmd/compare.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 NAME HERE
+// Copyright © 2018 Matthias Diester
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
